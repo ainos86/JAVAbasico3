0303f6233df67b3fa962df427258cbcf1ae9f6e0
ainos86 <116199341+ainos86@users.noreply.github.com>
2022-11-28 10:40:49 +0100

    first commit

---
+++ JAVAbasico3.java
@@ -0,0 +1,12 @@
+package JAVAbasico3;
+
+public class JAVAbasico3 {
+    public static void main(String[] args) {
+
+        String[] nombres = {"Pepe", "Juanito", "Luis", "Jose"};
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
+        }
+    }
+}
